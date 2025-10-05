5e708dbafbba29cbc3532b56de2b1c1ed7a04691
Alongkon Natphunwat <664259031@webmail.npru.ac.th>
2025-10-05 22:42:31 +0700

    .env

--- client/src/routers/index.jsx
+++ client/src/routers/index.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/Books",
+        path: "/books",
         element: <Books />,
       },
       {
