2518c810b270f0265c33120020bb5ad62465241d
Alongkon Natphunwat <664259031@webmail.npru.ac.th>
2025-10-10 19:57:50 +0700

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import router from "./routes/Router";
+import router from "./routers/Router";
 import { RouterProvider } from "react-router";
 import NavBar from "./components/Navbar";
 createRoot(document.getElementById("root")).render(
