9f9fd9a99bfd5b9afa000dd9a29c494c15c09652
Alongkon Natphunwat <664259031@webmail.npru.ac.th>
2025-10-10 21:29:51 +0700

    update

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router";
-import AddBook from "../pages/AddBook";
-import AddComic from "../pages/AddComic";
-import AddJournal from "../pages/AddJournal";
-import UpdateBook from "../pages/UpdateBook";
-import UpdateJournal from "../pages/UpdateJournal";
-import UpdateComic from "../pages/UpdateComic";
-import Books from "../pages/Books";
-import Journal from "../pages/Journals";
-import Comic from "../pages/Comics";
+import AddBook from "../pages/AddBook.jsx";
+import AddComic from "../pages/AddComic.jsx";
+import AddJournal from "../pages/AddJournal.jsx";
+import UpdateBook from "../pages/UpdateBook.jsx";
+import UpdateJournal from "../pages/UpdateJournal.jsx";
+import UpdateComic from "../pages/UpdateComic.jsx";
+import Books from "../pages/Books.jsx";
+import Journal from "../pages/Journals.jsx";
+import Comic from "../pages/Comics.jsx";
+
 const router = createBrowserRouter([
   {
     path: "/",
